4e66eceaec2b0b83ba1eb77e77cfd6652a712647
AIO1 <alejandroibrahimojea@gmail.com>
2025-10-03 14:11:50 +0200

    Remove SSR

--- Frontend/ECSPrimengTable/src/app/app.config.ts
+++ Frontend/ECSPrimengTable/src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
@@ -33,7 +32,6 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
-    provideClientHydration(withEventReplay()),
     provideHttpClient(
       withInterceptorsFromDi(),
       withFetch()
